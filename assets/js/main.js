fdca375eba51c3e83ee7234b1cbdd19388c6707e
ulrick <ymanasse@gmail.com>
2022-07-11 23:11:06 +0200

    style: add several animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
+var quotationText1 = new Typed('#quotation-text1', {
+    strings: ['<p>Diplômé en Génie Logiciel à l\'UTBM,</p>'+
+    '<p>avec 6 ans d\'expériences,</p>' +
+    '<p>aujourd\'hui je suis cadre en informatique chez LIEBHERR</p>' +
+    '<p><strong>Je suis^1000\n</strong></p>' +
+    '<h3>Ingénieur</h3>'],
+    fadeOut: true,
+    startDelay: 3000,
+    showCursor: false,
+    typeSpeed: 30
+});
+
+var quotationText2 = new Typed('#quotation-text2', {
+    strings: [
+    '<p>Je conçois du code et j\'en fais des choses extraordinaires:</p>' +
+    '<p>Des applications, logiciels et outils</p>' +
+    '<p>qui faciliteront votre quotidien.</p>' +
+    '<p><strong>Je suis^1000\n</strong></p><h3>Développeur</h3>'
+    ],
+    fadeOut: true,
+    startDelay: 12000,
+    showCursor: false,
+    typeSpeed: 30
+});
+                    
+
 var toTop = document.getElementById("to-top");
 window.onscroll = function() {
     if (window.pageYOffset > 1000) {
@@ -11,10 +37,26 @@ window.onscroll = function() {
 window.onload = () => {
     setTimeout(() => {
         document.getElementsByClassName("esprit")[0].style.visibility = "visible";
-    }, 2000);
+    }, 1000);
 }
 
 function goToPageTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var elementTop = reveals[i].getBoundingClientRect().top;
+  
+      if (elementTop < window.innerHeight - 100) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+window.addEventListener("scroll", reveal);
